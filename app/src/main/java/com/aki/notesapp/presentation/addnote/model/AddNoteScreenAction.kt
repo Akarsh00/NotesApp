05f441a69ce64cd1df75792ad5b3092ce7260eed
akarsh7 <akarsh@trulymadly.com>
2025-04-12 01:01:50 +0530

    Changes

--- app/src/main/java/com/aki/notesapp/presentation/addnote/model/AddNoteScreenAction.kt
+++ app/src/main/java/com/aki/notesapp/presentation/addnote/model/AddNoteScreenAction.kt
@@ -1,7 +1,9 @@
 package com.aki.notesapp.presentation.addnote.model
 
 sealed interface AddNotesScreenAction {
+
     data object OnSaveNoteAction : AddNotesScreenAction
     data class OnNotesTextChange(val note: NoteItem, val newNumber: String) : AddNotesScreenAction
     data object OnFabClicked : AddNotesScreenAction
+
 }
